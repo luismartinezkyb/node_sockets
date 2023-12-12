ccbe2a27c7383e4b2a324a9405ee68bb1cd6db88
luismartinezkyb <18030545@itcelaya.edu.mx>
2023-12-11 22:56:40 -0600

    (init): Inicializo app con socket.io js vanilla con Nodejs

---
+++ sockets/controller.js
@@ -0,0 +1,25 @@
+const socketController =(client)=>{
+  console.log('cliente conectado', client.id)
+  client.on('disconnect',()=>{
+    console.log('cliente desconectado')
+  })
+
+
+  client.on('enviar-mensaje', (message, callback)=>{
+    //ESTO ENVIA A SOLO EL EL MENSAJE
+    // client.emit('enviar-mensaje', message);
+    //ESTO EMITE A TODOS MENOS A EL
+    client.broadcast.emit('enviar-mensaje', message);
+
+    // ESTO EMITE A TODOS INCLUYENDOLO
+    // this.io.emit('enviar-mensaje', message);
+    const id =23123;
+    callback(id)
+    
+  })
+  
+}
+
+module.exports ={
+  socketController
+}
